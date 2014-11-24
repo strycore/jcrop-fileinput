1bba72a6fe06e4363c38cd5cc9d8b30369a1dc04
Mathieu Comandon <strycore@gmail.com>
2014-11-24 14:16:16 +0100

    Retab gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,49 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+  grunt.initConfig({
 
-		// Import package manifest
-		pkg: grunt.file.readJSON("jcrop-fileinput.json"),
+    // Import package manifest
+    pkg: grunt.file.readJSON("jcrop-fileinput.json"),
 
-		// Banner definitions
-		meta: {
-			banner: "/*\n" +
-				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
-				" *  <%= pkg.description %>\n" +
-				" *  <%= pkg.homepage %>\n" +
-				" *\n" +
-				" *  Made by <%= pkg.author.name %>\n" +
-				" *  Under <%= pkg.licenses[0].type %> License\n" +
-				" */\n"
-		},
+    // Banner definitions
+    meta: {
+      banner: "/*\n" +
+        " *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+        " *  <%= pkg.description %>\n" +
+        " *  <%= pkg.homepage %>\n" +
+        " *\n" +
+        " *  Made by <%= pkg.author.name %>\n" +
+        " *  Under <%= pkg.licenses[0].type %> License\n" +
+        " */\n"
+    },
 
-		// Lint definitions
-		jshint: {
-			files: ["src/jcrop-fileinput.js"],
-			options: {
-				jshintrc: ".jshintrc"
-			}
-		},
+    // Lint definitions
+    jshint: {
+      files: ["src/jcrop-fileinput.js"],
+      options: {
+        jshintrc: ".jshintrc"
+      }
+    },
 
-		// Minify definitions
-		uglify: {
-			my_target: {
-				src: ["dist/jcrop-fileinput.js"],
-				dest: "dist/jcrop-fileinput.min.js"
-			},
-			options: {
-				banner: "<%= meta.banner %>"
-			}
-		},
+    // Minify definitions
+    uglify: {
+      my_target: {
+        src: ["dist/jcrop-fileinput.js"],
+        dest: "dist/jcrop-fileinput.min.js"
+      },
+      options: {
+        banner: "<%= meta.banner %>"
+      }
+    },
 
-		// CoffeeScript compilation
-		coffee: {
-			compile: {
-				files: {
-					"dist/jcrop-fileinput.js": "src/jcrop-fileinput.coffee"
-				}
-			}
-		},
+    // CoffeeScript compilation
+    coffee: {
+      compile: {
+        files: {
+          "dist/jcrop-fileinput.js": "src/jcrop-fileinput.coffee"
+        }
+      }
+    },
 
     sass: {
       dist: {
@@ -87,15 +87,15 @@ module.exports = function(grunt) {
       }
     }
 
-	});
+  });
 
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-coffee");
-	grunt.loadNpmTasks("grunt-contrib-sass");
-	grunt.loadNpmTasks("grunt-contrib-watch");
-	grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-coffee");
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-connect");
 
-	grunt.registerTask("default", ["jshint", "coffee", "sass", "uglify"]);
-	grunt.registerTask("server", ["connect:livereload", "watch"]);
+  grunt.registerTask("default", ["jshint", "coffee", "sass", "uglify"]);
+  grunt.registerTask("server", ["connect:livereload", "watch"]);
 };
